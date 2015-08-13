a049e132ef61803584a7d409e754a15ea61edf05
Thirupathi Reddy Guduru <thirupathi.guduru@cerner.com>
2015-08-13 09:01:33 -0500

    0.2.0 cookbooks with attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -2,22 +2,57 @@
 # Cookbook Name:: apache
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2012, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
 
-# this one checks the chef for available Cookbook for httpd and then install on the node,
-# this should be name of the package available from OS specific package libraries like apt-get,yum etc....
 package "apache2" do
   action :install
 end
 
 service "apache2" do
-  action [ :enable, :start ]
+  action [ :start, :enable ]
 end
 
-cookbook_file "/var/www/index.html" do
-  source "index.html"
-  mode "0644"
+execute "a2dissite default" do
+  only_if do
+    File.symlink?("/etc/apache2/sites-enabled/000-default")
+  end
+  notifies :restart, "service[apache2]"
+end
+
+node['apache']['sites'].each do |site_name, site_data|
+  document_root = "/srv/apache/#{site_name}"
+
+  template "/etc/apache2/sites-available/#{site_name}" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data['port']
+    )
+    notifies :restart, "service[apache2]"
+  end
+
+  execute "a2ensite #{site_name}" do
+    not_if do
+      File.symlink?("/etc/apache2/sites-enabled/#{site_name}")
+    end
+    notifies :restart, "service[apache2]"
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port']
+    )
+    mode "0644"
+  end
 end
